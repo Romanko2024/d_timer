ac73ec261dc481be4f7e09e3c69c2ff69ef7d1a5
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-02-23 13:29:51 +0200

    action & interval

--- d_timer/Program.cs
+++ d_timer/Program.cs
@@ -1,8 +1,19 @@
 ﻿using System.Threading;
 
 class Timer
-{ 
-
+{
+    //посилання на метод 
+    private readonly Action action;
+    //інтвервал між виконанням action
+    private readonly int interval;
+    //конструктор класу
+    public Timer(Action с_action, int с_interval) //метод для виконання, інтервал.
+    {
+        //якщо с_action == null кидає виняток
+        action = с_action ?? throw new ArgumentNullException(nameof(с_action));
+        //кидає виняток при с_interval менше 0
+        interval = с_interval > 0 ? с_interval : throw new ArgumentException("Інтервал має бути більше 0 секунд");
+    }
 }
 
 class Program
