16d457bb47aecd28dfb780bc5fd44db903d239d1
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-02-23 13:42:29 +0200

    Start

--- d_timer/Program.cs
+++ d_timer/Program.cs
@@ -6,6 +6,10 @@ class Timer
     private readonly Action action;
     //інтвервал між виконанням action
     private readonly int interval;
+    //флаг таймер працює/ні
+    private bool isRunning;
+    //потік таймера
+    private Thread thread;
     //конструктор класу
     public Timer(Action с_action, int с_interval) //метод для виконання, інтервал.
     {
@@ -13,6 +17,19 @@ class Timer
         action = с_action ?? throw new ArgumentNullException(nameof(с_action));
         //кидає виняток при с_interval менше 0
         interval = с_interval > 0 ? с_interval : throw new ArgumentException("Інтервал має бути більше 0 секунд");
+    }
+    //запуск таймера
+    public void Start()
+    {
+        if (isRunning) return; //нічого не робимо якщо уже запущений
+        isRunning = true; //перемикаємо флаг
+        //новий потік в якому проходить метод безпосередьної роботи таймера
+        thread = new Thread(Run) { IsBackground = true }; // IsBackground = true - зупиниться при закритті програми
+        thread.Start();
+    }
+    private void Run()
+    {
+        
     }
 }
 
