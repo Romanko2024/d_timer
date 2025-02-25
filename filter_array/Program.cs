067e5f1364d7a73a4f46ac2ac542f766285dabf3
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-02-25 16:25:40 +0200

    filter_array_base

---
+++ filter_array/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //початковий масив
+        int[] numbers = { 10, 15, 20, 25, 30, 35, 40, 45, 50 };
+        Console.Write("Введіть число відносно якого буде перевірятися кратність: ");
+        int k = int.Parse(Console.ReadLine());
+    }
+}
