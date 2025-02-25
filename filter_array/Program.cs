f645bbcb6aa89620c5ee00fb2eb23896fe0e59ed
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-02-25 16:45:09 +0200

    FilterDelegate

--- filter_array/Program.cs
+++ filter_array/Program.cs
@@ -1,10 +1,14 @@
 ﻿class Program
 {
+    //делегат на який посилаються методи(лямбда вирази)
+    delegate bool FilterDelegate(int number, int k); //прийма числа викидає так/ні
     static void Main()
     {
         //початковий масив
         int[] numbers = { 10, 15, 20, 25, 30, 35, 40, 45, 50 };
         Console.Write("Введіть число відносно якого буде перевірятися кратність: ");
         int k = int.Parse(Console.ReadLine());
+        //прив'язуємо лямбда вираз до змінної типу створеного делегата 
+        FilterDelegate isMultiple = (number, divisor) => number % divisor == 0;
     }
 }
