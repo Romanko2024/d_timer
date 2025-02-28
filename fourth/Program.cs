d24619f1af2490a4abec3c8d064c357b9bd54e03
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-02-28 18:44:57 +0200

    try_catch

--- fourth/Program.cs
+++ fourth/Program.cs
@@ -4,5 +4,21 @@
     {
         Console.WriteLine("Вводьте рядки у форматі: <функція> <число>");
         Console.WriteLine("Функції: 0 - sqrt(abs(x)), 1 - x^3, 2 - x + 3.5");
+        while (true)
+        {
+            try
+            {
+                //зчитати рядок
+                //викликати виконання оепрацыъ
+            }
+            catch
+            {
+                Console.WriteLine($"Сталася помилка: {ex.Message}");
+                //Console.WriteLine("Бажаємо всього найкращого!");
+                //Console.WriteLine("Щасливої дороги!");
+                break;
+            }
+        }
     }
+
 }
