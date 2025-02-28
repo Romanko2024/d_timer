be3ef0bc0978d7daf245b77322ee7559b23136c7
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-02-28 19:05:28 +0200

    lambda func arr

--- fourth/Program.cs
+++ fourth/Program.cs
@@ -4,6 +4,15 @@
     {
         Console.WriteLine("Вводьте рядки у форматі: <функція> <число>");
         Console.WriteLine("Функції: 0 - sqrt(abs(x)), 1 - x^3, 2 - x + 3.5");
+
+        // МАСИВ лямбда функцій
+        Func<double, double>[] operations = 
+            {
+            x => Math.Sqrt(Math.Abs(x)),
+            x => x * x * x,
+            x => x + 3.5
+            };
+
         while (true)
         {
             try
@@ -11,7 +20,7 @@
                 var input = Console.ReadLine();
                 var values = input.Split().Select(double.Parse).ToArray();
                 //викликати виконання оепрацыъ через індексв масиві лабда функцій що тепер треба зробити..
-                Console.WriteLine(whatever[(int)values[0]](values[1]));
+                Console.WriteLine(operations[(int)values[0]](values[1]));
             }
             catch (Exception ex)
             {
@@ -22,5 +31,4 @@
             }
         }
     }
-
 }
