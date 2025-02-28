0f775dabcff4a7a761e60faef9f46ba01e05562a
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-02-28 15:11:52 +0200

    FirstSeries

--- Series/Program.cs
+++ Series/Program.cs
@@ -1,6 +1,8 @@
 ﻿class Program
 {
 
+    //рахуємо ітий член першого ряду. і треба якось через делегат..
+    static double FirstSeries(int i) => 1.0 / Math.Pow(2, i - 1);
     static void Main() 
     { 
 
