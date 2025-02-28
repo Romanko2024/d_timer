f379f2e8caa936ff39499bbf14d64438d26e2f62
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-02-28 18:32:16 +0200

    fourth skeleton

---
+++ fourth/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Вводьте рядки у форматі: <функція> <число>");
+        Console.WriteLine("Функції: 0 - sqrt(abs(x)), 1 - x^3, 2 - x + 3.5");
+    }
+}
