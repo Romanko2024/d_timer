4f5ca998c858cb804a536a33325c086bff956703
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-02-28 15:23:57 +0200

    SecondSeries

--- Series/Program.cs
+++ Series/Program.cs
@@ -3,6 +3,23 @@
 
     //рахуємо ітий член першого ряду. і треба якось через делегат..
     static double FirstSeries(int i) => 1.0 / Math.Pow(2, i - 1);
+
+    //рахуємо ітий член другого ряду
+    static double SecondSeries(int i)
+    {
+        double factorial = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            //j=1 factorial= 1*1=1
+            //j=2 factorial= 1*2=2
+            //j=3 factorial= 2*3=6
+            factorial *= j;
+        }
+        return 1.0 / factorial;
+    }
+
+    //рахуємо ітий член другого ряду
+
     static void Main() 
     { 
 
