aa42287c9ceb282dd8cbe7b9005406500bae35cc
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-03-07 13:48:03 +0200

    SelectionSortReference

--- Sort_tester/Program.cs
+++ Sort_tester/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿class SortingTester
+{
+    // Еталонні методи сортування
+    static void SelectionSortReference(int[] array)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < array.Length; j++)
+                if (array[j] < array[minIndex])
+                    minIndex = j;
+
+            (array[i], array[minIndex]) = (array[minIndex], array[i]);
+        }
+    }
+    //
+}
