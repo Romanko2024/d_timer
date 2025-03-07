ee9fd480aeabf15324b4e2543b4c2d77e2ea35a9
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-03-07 15:12:56 +0200

    ShakerSortReference

--- Sort_tester/Program.cs
+++ Sort_tester/Program.cs
@@ -13,5 +13,46 @@
             (array[i], array[minIndex]) = (array[minIndex], array[i]);
         }
     }
+    static void ShakerSortReference(int[] array)
+    {
+        int left = 0, right = array.Length - 1;
+        bool swapped;
+
+        do
+        {
+            swapped = false;
+            int lastSwapIndex = right;
+
+            //прохід зліва направо
+            for (int i = left; i < right; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    (array[i], array[i + 1]) = (array[i + 1], array[i]);
+                    swapped = true;
+                    lastSwapIndex = i;
+                }
+            }
+            right = lastSwapIndex;
+
+            if (!swapped) break;//вихід if не було обмінів
+
+            swapped = false;
+            lastSwapIndex = left;
+
+            //прохід справа наліво
+            for (int i = right; i > left; i--)
+            {
+                if (array[i] < array[i - 1])
+                {
+                    (array[i], array[i - 1]) = (array[i - 1], array[i]);
+                    swapped = true;
+                    lastSwapIndex = i;
+                }
+            }
+            left = lastSwapIndex;
+
+        } while (swapped);
+    }
     //
 }
