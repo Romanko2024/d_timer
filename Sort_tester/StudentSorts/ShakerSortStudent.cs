9a0a29859bc37e5bd674dbd30d2b426c926362cd
Roman <manko.roman1123@vu.cdu.edu.ua>
2025-03-16 12:27:38 +0200

    shakerstud

--- Sort_tester/StudentSorts/ShakerSortStudent.cs
+++ Sort_tester/StudentSorts/ShakerSortStudent.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sort_tester.StudentSorts
+public static class StudentSorts
 {
-    internal class ShakerSortStudent
+    public static void ShakerSort(int[] array)
     {
+        int left = 0, right = array.Length - 1;
+        while (left < right)
+        {
+            for (int i = left; i < right; i++)
+                if (array[i] > array[i + 1])
+                    (array[i], array[i + 1]) = (array[i + 1], array[i]);
+
+            right--;
+
+            for (int i = right; i > left; i--)
+                if (array[i] < array[i - 1])
+                    (array[i], array[i - 1]) = (array[i - 1], array[i]);
+
+            left++;
+        }
     }
 }
